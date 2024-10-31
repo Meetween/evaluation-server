378cafc1077348d51b1388b23ef103d3439419ca
Roldano Cattoni <cattoni@fbk.eu>
2024-10-31 14:55:57 +0100

    add SQA-HF-metrics

---
+++ run-SQA-HF-metrics__ares.sh
@@ -0,0 +1,68 @@
+#! /bin/bash
+
+#SBATCH -A plgmeetween2004-cpu
+#SBATCH -p plgrid
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=1G
+#SBATCH --job-name=sqaHF
+#SBATCH --time 72:00:00
+
+# -----------
+# manage args
+# -----------
+
+print_if_verbose() {
+  if test $verbose -eq 1 ; then echo "$@" 1>&2 ; fi
+}
+
+show_help() {
+  cat << EOF
+ARGS: [-h] [-v] lang hypFile refFile
+  where
+      -h        print help
+      -v        verbose
+      lang      two-digit language code
+      hypFile   json file with answer predictions
+      refFile   json file with questtion-answer references
+EOF
+}
+
+
+# A POSIX variable
+OPTIND=1         # Reset in case getopts has been used previously in the shell.
+
+# Initialize our own variables:
+verbose=0
+debugInfo=""
+
+while getopts "hv" opt; do
+  case "$opt" in
+    h)
+      show_help
+      exit 0
+      ;;
+    v)
+      verbose=1
+      debugInfo='-d'
+      ;;
+  esac
+done
+
+shift $((OPTIND-1))
+
+[ "${1:-}" = "--" ] && shift
+
+lang=$1
+hyp=$2
+ref=$3
+shift 3
+
+test -f "$hyp" || { echo cannot find hyp $hyp ; exit 1 ; }
+test -f "$ref" || { echo cannot find ref $ref ; exit 1 ; }
+
+exe=${PLG_GROUPS_STORAGE}/plggmeetween/envs/etc/HF/compute_score.py
+
+python3 $exe $debugInfo $ref $hyp 2> /dev/null
+
+
