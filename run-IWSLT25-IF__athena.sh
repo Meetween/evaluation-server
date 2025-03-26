d09ec0226c989bd6834c49ff07d517566c4cfbf8
Roldano Cattoni <cattoni@fbk.eu>
2025-03-26 15:29:50 +0100

    add IWSLT25 Instruction-Following shared task eval

---
+++ run-IWSLT25-IF__athena.sh
@@ -0,0 +1,87 @@
+#! /bin/bash
+
+#SBATCH -A plgmeetween2025-gpu-a100
+#SBATCH -p plgrid-gpu-a100
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:1
+#SBATCH --mem=50G
+#SBATCH --job-name=IF
+
+# script for the evaluation of instruction following (IWSLT2025 shared task)
+
+
+# -----------
+# manage args
+# -----------
+
+show_help() {
+  cat << EOF
+ARGS: [-h] [-n] srcL tgtL hypFile refFile
+  where
+      -h	print help
+      -n	do NOT perform re-segmentation of hypFile
+EOF
+}
+
+
+# A POSIX variable
+OPTIND=1         # Reset in case getopts has been used previously in the shell.
+
+# Initialize our own variables:
+resegment=1
+
+while getopts "hn" opt; do
+  case "$opt" in
+    h)
+      show_help
+      exit 0
+      ;;
+    n)
+      resegment=0
+      ;;
+  esac
+done
+
+shift $((OPTIND-1))
+
+[ "${1:-}" = "--" ] && shift
+
+test "$#" -ge 4 || { show_help ; exit 1 ; }
+scrLang=$1
+tgtLang=$2
+hypFile=$3
+refFile=$4
+shift 4
+
+test -f "$hypFile" || { echo cannot find hypFile $hypFile ; exit 1 ; }
+test -f "$refFile" || { echo cannot find refFile $refFile ; exit 1 ; }
+
+# TODO: Roldano source ENV
+source ${PLG_GROUPS_STORAGE}/plggmeetween/envs/setup/ifeval.USE
+
+# TODO: Roldano mettere la cartella in cui si trova l'eseguibile "mwerSegmenter"
+export MWERSEGMENTER_ROOT=${PLG_GROUPS_STORAGE}/plggmeetween/envs/etc/mwerSegmenter/bin
+
+# TODO: Roldano metti il path corretto al file python
+exe=${PLG_GROUPS_STORAGE}/plggmeetween/envs/etc/IF/evaluation.py
+
+track=short
+if echo $refFile | grep -i long &> /dev/null ; then track=long ; fi
+
+python $exe -s $hypFile -r $refFile -t $track -l $tgtLang
+
+
+exit
+
+
+# mockup 
+state="OK"
+werS="22.2"
+cometS="0.666"
+sqaS="0.456"
+ssumS="0.789"
+printf '{"state": "%s", "scores": {"ASR-WER": %s, "ST-COMET": %s, "SQA-BERTScore": %s, "SSUM-BERTScore": %s}}\n' $state $werS $cometS $sqaS $ssumS
+
+
+
